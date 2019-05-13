1dea872eddecf984f0590e760f395b4ffa1bd51e
shane feng <shanevi29@gmail.com>
2019-05-12 20:07:59 -0700

    Event Info HTML/CSS Final

--- public/js/events.js
+++ public/js/events.js
@@ -10,7 +10,6 @@ $("#volunteerTab").click(function () {
     $("#volunteerTab").css({"borderBottom": "4px solid #3498db", "color": "black"});
     $("#appliedTab").css({"borderBottom": "none", "color": "grey"});
     $("#eventInfoBox").hide();
-    $("#eventInfoBox").hide();
     $("#eventsBox").show();
 });
 
@@ -19,13 +18,14 @@ $("#appliedTab").click(function () {
     $("#appliedTab").css({"borderBottom": "4px solid #3498db", "color": "black"});
     $("#volunteerTab").css({"borderBottom": "none", "color": "grey"});
     $("#eventsBox").hide();
+    $("#eventInfoBox").hide();
     $("#joinedEventsBox").show();
 });
 
 //MENU DROP DOWN---------------------------------------------------->
-$("#menuButton").click(function () {
+$(".menuButton").click(function () {
     $("#dropDownBox").toggle();
-})
+});
 
 //MORE DETAILS BUTTON----------------------------------------------------------
 $("#readMoreButton").click(function () {
