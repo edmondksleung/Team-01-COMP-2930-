fed02848d8ba126ddaab0b83fd3388f8cd6026f0
victorwu <wuvic93@gmail.com>
2019-05-14 14:03:50 -0700

    updated branch

--- public/js/eventDetail.js
+++ public/js/eventDetail.js
@@ -1,15 +1,60 @@
-let database = firebase.database();
-// Fetching URL parameter
-let params = (new URL(window.location)).searchParams;
-let city = params.get("city");
-let key = params.get("key");
-// console.log(city);
-// console.log(key);
-let dbref = database.ref(`events/${city + "/" + key}`);
-// Display details of the events on the page
-dbref.on('value', function (snapshot) {
-  document.getElementById('subject').innerHTML = (snapshot.val().subject);
-  document.getElementById('email').innerHTML = (snapshot.val().email);
-  document.getElementById('city').innerHTML = (snapshot.val().city);
-  document.getElementById('content').innerHTML = (snapshot.val().content);
+$(document).ready(function(){
+  let database = firebase.database();
+  // Fetching URL parameter
+  let params = (new URL(window.location)).searchParams;
+  let city = params.get("city");
+  let key = params.get("key");
+  // console.log(city);
+   console.log(key);
+  let dbref = database.ref(`events/${city + "/" + key}`);
+  console.log(dbref);
+  // Display details of the events on the page
+  dbref.on('value', function (snapshot) {
+    document.getElementById('subject').innerHTML = (snapshot.val().subject);
+    document.getElementById('email').innerHTML = (snapshot.val().email);
+    document.getElementById('city').innerHTML = (snapshot.val().city);
+    document.getElementById('content').innerHTML = (snapshot.val().content);
+  });
+    // Creates a new variable "anchor"
+    //var anchor = $("<a></a>");
+    // Giving unique id to url(template literal) & and passes it to next page
+    //anchor.attr('href', `./edit.html?city=${city}&key=${key}`);
+    //$("body").append(anchor);
+
+    $("#update").attr('href', `./edit.html?city=${city}&key=${key}`);
+
+    // Creates a new variable "button" using jQuery 
+    // var btn = $("<button></button>");
+    // anchor.append(btn);
+    // btn.append("edit");
+
+
+
+    // Adds the button element within same, but before <p> tag as "delete"
+    // var dlt = $("<button></button");
+    //dlt.attr("id", "delete");
+    //dlt.append("Delete");
+    //$("body").append(dlt);
+
+    //$("#delete").before(btn);
+
+  
+    // Removes database child node using delete button within eventDetail.html 
+    // Uses jQuery to reference 
+    $("#delete").on("click", function(){
+      //alert("success"); 
+      // Removes the post front-end and in database
+      dbref.remove();
+      //Directs back to list of cities.html
+      setTimeout(function(){
+        window.location.href= "../public/cities.html";
+      }, 1000);
+    });
+
+
+
+
+
+
+
 });
--- public/js/eventList.js
+++ public/js/eventList.js
@@ -2,17 +2,17 @@ let database = firebase.database();
 // Fetching and assigning URL parameter
 let params = (new URL(document.location)).searchParams;
 let cityName = params.get("city");
-
 // console.log(cityName);
 let dbref = database.ref(`events/${cityName}`);
 dbref.on('value', gotData, errData);
-
+//console.log(dbref);
 // Grabs data from firebase
 function gotData(data) {
-  // console.log(data.val());
+   //console.log(data.val());
   let events = data.val();
+    console.log(events);
   let keys = Object.keys(events);
-  // console.log(keys);
+   console.log(keys);
 
   // Loop that creates list for each event
   for (let i = 0; i < keys.length; i++){
@@ -20,12 +20,14 @@ function gotData(data) {
     let event = events[k].subject;
     console.log(k);
     let list = document.createElement('li');
+    console.log(list);
     // Giving attibute to list that redirects to the detail page
     let link = document.createElement('a');
     // Giving unique id to url(template literal)
     link.setAttribute('href', `./eventDetail.html?city=${cityName}&key=${k}`);
     // Append event data in the anchor
     link.append(event);
+    console.log(link);
     // Append anchor in the list
     list.append(link);
     let eventsList = document.getElementById('eventsList');
