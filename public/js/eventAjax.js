765089741e7bf3bbfca854d62a3c1742b6ea245b
Paul Kim <paulgmkim@gmail.com>
2019-05-14 22:38:08 -0700

    anchor removed

--- public/js/eventAjax.js
+++ public/js/eventAjax.js
@@ -1,4 +1,6 @@
+// AJAX to import data from firebase
 $('.cities').click(function () {
+    // Grabbing clicked name attribute
     let city = $(this).attr('name');
     $.ajax({
         type: 'GET',
@@ -6,9 +8,9 @@ $('.cities').click(function () {
         dataType: 'json',
         success: function (data) {
             document.getElementById('location').innerHTML = city.toUpperCase();
-            console.log(Object.keys(data));
             let keys = Object.keys(data);
-
+            console.log(keys);
+            // Creating a list of events from firebase
             for (let i = 0; i < keys.length; i++) {
                 let k = keys[i];
                 let event = data[k].subject;
