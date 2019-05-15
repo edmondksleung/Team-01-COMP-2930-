672bd99b92046da9ea6f42a4137a03e8d37ba1e5
Paul <paulgmkim@gmail.com>
2019-05-14 21:13:20 -0700

    ajax implemented to fetch data

--- public/js/transition.js
+++ public/js/transition.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-$(".cities").on('click', function (e) {
-  $(".subPageWrapper").fadeOut(500);
-=======
 $(".cities").click(function () {
   $(".welcomePageWrapper").fadeOut(500);
->>>>>>> fb929859e4cf4d3ebdbc10fa535a5ff0e34e72d7
   setTimeout(function () {
     $('#eventsWrapper').fadeIn(500);
   }, 1000);
