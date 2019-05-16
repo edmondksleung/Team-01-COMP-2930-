addf6d5a4abdb379de00d7618fa394267c1ca39c
shane feng <shanevi29@gmail.com>
2019-05-16 13:16:01 -0700

    index.html eventsInfoBox fixed completely

--- public/js/events.js
+++ public/js/events.js
@@ -1,58 +1,58 @@
 //SHOW EVENTS WRAPPER PAGE---------------------------------------------
 $(".cityImage h1").click(function () {
     $(".mainPageWrapper").fadeOut(500);
-    $("#eventsWrapper").fadeIn(1500);
-    $("#dropDownBox").hide();
+    $(".eventsWrapper").fadeIn(1500);
+    $(".dropDownBox").hide();
     $(document.body).css("background", "white");
 });
 
 //EVENTS PAGE ON LOAD SHOW VOLUNTEER PAGE---------------------------------
-$("#eventsWrapper").ready(function () {
-    $("#eventsInfoBox").hide();
+$(".eventsWrapper").ready(function () {
+    $(".eventsInfoBox").hide();
     $(".joinedEventsBox").hide();
     $(".eventsBox").show();
 });
 
 //SHOW VOLUNTEER TAB---------------------------------------------------------
-$("#volunteerTab").click(function () {
-    $("#volunteerTab").css({
-        "borderBottom": "4px solid #3498db",
+$(".volunteerTab").click(function () {
+    $(".volunteerTab").css({
+        "borderBottom": "4px solid .3498db",
         "color": "black"
     });
-    $("#appliedTab").css({
+    $(".appliedTab").css({
         "borderBottom": "none",
         "color": "grey"
     });
-    $("#eventsInfoBox").hide();
+    $(".eventsInfoBox").hide();
     $(".joinedEventsBox").hide();
     $(".joinedEventsInfoBox").hide();
     $(".eventsBox").show();
 });
 
 //SHOW JOINED TAB---------------------------------------------------------
-$("#appliedTab").click(function () {
-    $("#appliedTab").css({
-        "borderBottom": "4px solid #3498db",
+$(".appliedTab").click(function () {
+    $(".appliedTab").css({
+        "borderBottom": "4px solid .3498db",
         "color": "black"
     });
-    $("#volunteerTab").css({
+    $(".volunteerTab").css({
         "borderBottom": "none",
         "color": "grey"
     });
     $(".eventsBox").hide();
-    $("#eventsInfoBox").hide();
+    $(".eventsInfoBox").hide();
     $(".joinedEventsInfoBox").hide();
     $(".joinedEventsBox").show();
 });
 
 //MENU DROP DOWN----------------------------------------------------
 $(".menuButton").click(function () {
-    $("#dropDownBox").toggle();
+    $(".dropDownBox").toggle();
 });
 
 //BACK BUTTON FUNCTION-------------------------------------------------------
 $(".backButton").click(function () {
-    $("#eventsWrapper").fadeOut(1000);
+    $(".eventsWrapper").fadeOut(1000);
     $(".subPageWrapper").fadeIn(1000);
 });
 
@@ -60,7 +60,7 @@ $(".backButton").click(function () {
 $(".readMoreButton").click(function () {
     $(".eventsBox").hide();
     $(".joinedEventsBox").hide();
-    $("#eventsInfoBox").show();
+    $(".eventsInfoBox").show();
 });
 
 
@@ -72,15 +72,15 @@ $(".joinedReadMoreButton").click(function () {
 });
 
 //CANCEL DETAILS WINDOW BUTTON---------------------------------------
-$("#cancelButton").click(function () {
-    $("#eventsInfoBox").hide();
-    $("#eventInfoBox").hide();
-    $("#eventsBox").show();
+$(".cancelButton").click(function () {
+    $(".eventsInfoBox").hide();
+    $(".eventInfoBox").hide();
+    $(".eventsBox").show();
 });
 
 //HOME BUTTON-------------------------------------------------------------------
 $(".homeButton").click(function() {
-    $("#eventsWrapper").hide();
+    $(".eventsWrapper").hide();
     $(".subPageWrapper").fadeIn(1000);
     $(document.body).css("background", "rgb(33, 49, 92)");
 
@@ -89,14 +89,14 @@ $(".homeButton").click(function() {
 
 //ABOUT US BUTTON-------------------------------------------------------------------
 $(".aboutButton").click(function() {
-    $("#eventsWrapper").fadeOut(1000);
+    $(".eventsWrapper").fadeOut(1000);
     $(".aboutUsWrapper").fadeIn(1000);
     $(document.body).css("background", "rgb(33, 49, 92)");
 });
 
 //LOCATION BUTTON-------------------------------------------------------------------
 $(".locationButton").click(function() {
-    $("#eventsWrapper").fadeOut(1000);
+    $(".eventsWrapper").fadeOut(1000);
     $(".mainPageWrapper").fadeIn(1000);
     $(document.body).css("background", "rgb(33, 49, 92)");
 });
