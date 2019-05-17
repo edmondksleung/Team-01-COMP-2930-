649d84702ba22dc8d9593e552318a580b02bd518
shane feng <shanevi29@gmail.com>
2019-05-17 12:24:45 -0700

    info details box/ close details button html/css/js

--- public/js/events.js
+++ public/js/events.js
@@ -1,81 +1,22 @@
-//SHOW EVENTS WRAPPER PAGE---------------------------------------------
-$(".cityImage h1").click(function () {
-    $(".mainPageWrapper").fadeOut(500);
-    $(".eventsWrapper").fadeIn(1500);
-    $(".dropDownBox").hide();
-    $(document.body).css("background", "white");
-});
-
-//EVENTS PAGE ON LOAD SHOW VOLUNTEER PAGE---------------------------------
-$(".eventsWrapper").ready(function () {
-    $(".eventsInfoBox").hide();
-    $(".joinedEventsBox").hide();
-    $(".eventsBox").show();
-});
-
-//SHOW VOLUNTEER TAB---------------------------------------------------------
-$(".volunteerTab").click(function () {
-    $(".volunteerTab").css({
-        "borderBottom": "4px solid .3498db",
-        "color": "black"
-    });
-    $(".appliedTab").css({
-        "borderBottom": "none",
-        "color": "grey"
-    });
-    $(".eventsInfoBox").hide();
-    $(".joinedEventsBox").hide();
-    $(".joinedEventsInfoBox").hide();
-    $(".eventsBox").show();
-});
-
-//SHOW JOINED TAB---------------------------------------------------------
-$(".appliedTab").click(function () {
-    $(".appliedTab").css({
-        "borderBottom": "4px solid .3498db",
-        "color": "black"
-    });
-    $(".volunteerTab").css({
-        "borderBottom": "none",
-        "color": "grey"
-    });
-    $(".eventsBox").hide();
-    $(".eventsInfoBox").hide();
-    $(".joinedEventsInfoBox").hide();
-    $(".joinedEventsBox").show();
-});
-
 //MENU DROP DOWN----------------------------------------------------
 $(".menuButton").click(function () {
     $(".dropDownBox").toggle();
-});
-
-//BACK BUTTON FUNCTION-------------------------------------------------------
-$(".backButton").click(function () {
-    $(".eventsWrapper").fadeOut(1000);
-    $(".subPageWrapper").fadeIn(1000);
 });
 
 //MORE DETAILS BUTTON----------------------------------------------------------
 $(".readMoreButton").click(function () {
     $(".eventsBox").hide();
     $(".joinedEventsBox").hide();
+    $(".eventsLocation").hide();
     $(".eventsInfoBox").show();
 });
 
-
-//ALREADY JOINED EVENTS MORE DETAILS BUTTON---------------------------------------
-$(".joinedReadMoreButton").click(function () {
-    $(".eventsBox").hide();
-    $(".joinedEventsBox").hide();
-    $(".joinedEventsInfoBox").show();
-});
-
 //CANCEL DETAILS WINDOW BUTTON---------------------------------------
-$(".cancelButton").click(function () {
+$(".closeDetailsButton").click(function () {
     $(".eventsInfoBox").hide();
-    $(".eventInfoBox").hide();
     $(".eventsBox").show();
+    $(".joinedEventsBox").show();
+    $(".eventsLocation").show();
 });
 
 //HOME BUTTON-------------------------------------------------------------------
