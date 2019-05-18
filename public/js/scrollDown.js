2fdae88c896ebb1ec4fdc08a9e6a763d3a3e2864
Edmond Leung <edmondksleung@hotmail.com>
2019-05-17 17:34:07 -0700

    Integrated About Us Page into index.html

--- public/js/scrollDown.js
+++ public/js/scrollDown.js
@@ -1,8 +1,16 @@
-    //Upon clicking the clickButton to view cities button on welcome page, scrolls down
-        $("#clickButton").click(function () {
-            $('html, body').animate({
-                scrollTop: $("#breakPoint").offset().top
-            }, 500);
-        });
+//Upon clicking the clickButton to view cities button on welcome page, scrolls down
+$("#clickButton").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#breakPoint").offset().top
+    }, 500);
+});
 
-    
+//Upon clicking meet the team button, the page will scroll down to the bios of team members
+$("#aboutUsPageClickButton").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#aboutUsPageBreakPoint").offset().top
+    }, 500);
+});
+
+
+
