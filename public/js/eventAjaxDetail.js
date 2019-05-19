3e3768fac531d45bbe569bd41a1bf5e56f891cfc
victorwu <wuvic93@gmail.com>
2019-05-18 19:49:37 -0700

    event details /before/ ajax calls

--- public/js/eventAjaxDetail.js
+++ public/js/eventAjaxDetail.js
@@ -1,28 +1,10 @@
-// AJAX to import data from firebase
-$('.cities').click(function () {
-    // Grabbing clicked name attribute
-    let city = $(this).attr('name');
-    console.log(city);
-    $.ajax({
-        type: 'GET',
-        url: `https://evolunteer-45c5d.firebaseio.com/events/${city}.json`,
-        dataType: 'json',
-        success: function (data) {
-            let keys = Object.keys(data);
-            console.log(keys);
-
-            // Creating a list of events from firebase
-            for (let i = 0; i < keys.length; i++) {
-                // Putting value into dom element
-                let k = keys[i];
-                console.log(k);
-                ////////// Creating event detail using dom elements /////////
-                
-
-            }
-            
-            // Increases count on join click
-        }
+$(document).ready(function () {
+    $(document).on("click", ".readMoreButton", function () {
+        // alert(this.id);
+        console.log("success");
+        // $(".eventsBox").hide();
+        // $(".joinedEventsBox").hide();
+        // $(".eventsLocation").hide();
+        // $(".eventsInfoBox").show();
     });
-
 });
