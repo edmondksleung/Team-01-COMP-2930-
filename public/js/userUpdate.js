89cde83137e64dca64b93ae69024de754ac43590
Paul Kim <paulgmkim@gmail.com>
2019-05-20 17:09:16 -0700

    user status update

--- public/js/userUpdate.js
+++ public/js/userUpdate.js
@@ -2,3 +2,7 @@
 
 let db = firebase.database();
 db.ref('users/ybIbLUTpOFYGRuex1lLDOODpaZA3').update({ isOrg: true});
+db.ref('users/abVZevFOBgg9xKimUN18VxsRnyj2').update({ isOrg: true});
+db.ref('users/UixX0NmSvRYMid8SYak3xh5qm2d2').update({ isOrg: true});
+db.ref('users/XKrGwDn60haoCeaEoukjqcjlc0T2').update({ isOrg: true});
+db.ref('users/ljgzbnvrKrTyw73R0nN7loi2AWs1').update({ isOrg: true});
