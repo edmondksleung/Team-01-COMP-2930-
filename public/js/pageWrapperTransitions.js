bf5d06b7eeea7c58c17cb174f0cb2e46cecc6534
eric kim <kimeric97@hotmail.com>
2019-05-22 16:02:32 -0700

    Transition js delete and move

--- public/js/pageWrapperTransitions.js
+++ public/js/pageWrapperTransitions.js
@@ -123,3 +123,11 @@
             $("body").css("background", "#B68D1B");
             $(".dropButtons").css("background", "#B68D1B");
         });
+        $(".cities").click(function () {
+            $(".welcomePageWrapper").fadeOut(500);
+            setTimeout(function () {
+              $('.eventsWrapper').fadeIn(500);
+            }, 1000);
+            $('.eventsWrapper').css('display', 'block');
+            $(".dropButtons").css("background", "#2f9e3a");
+          });
